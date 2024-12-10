d0a35543ec90aca47bba2de0b34c706962b9dec7
Gaurav Singh <133798269+primalasur@users.noreply.github.com>
2024-12-10 12:54:59 +0530

    Add files via upload

---
+++ contracts/Voting.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public hasVoted;
+
+    uint public candidatesCount;
+
+    event Voted(address indexed voter, uint indexed candidateId);
+
+    constructor() {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+}
